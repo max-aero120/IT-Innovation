7a572962663cc20724ccaf2ce75e413d727d57de
Max Strehle <max-aero120@users.noreply.github.com>
2015-10-14 23:33:40 -0500

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,36 @@
+#Returns Sum
+def add(x,y):
+    return x + y
+#Returns Differnce
+def sub(x,y):
+    return x - y
+#Returns Product
+def mul(x,y):
+    return x * y
+#Returns Quotient
+def div(x,y):
+    return x / y
+
+def main():
+    operation= input("What do you want to do? (+,-,*,/)")
+
+    if(operation != "+" and operation != "-" and operation != "*" and operation != "/"):
+        print ("You must enter a valid operation.")
+
+    else:
+        num1 = int(input("Enter First Number:"))
+        num2= int(input("Enter Second Number:"))
+
+        if(operation == "+"):
+            print (add(num1,num2))
+
+        elif(operaion == "-"):
+            print (sub(num1,num2))
+
+        elif(operation == "*"):
+            print (mul(num1,num2))
+
+        else:
+            print (Dic(num1,num2))
+
+main()
