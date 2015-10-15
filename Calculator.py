5d74a3053970220cec5d473988813ecd016a4027
Max Strehle <max-aero120@users.noreply.github.com>
2015-10-15 07:47:20 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -10,27 +10,33 @@ def mul(x,y):
 #Returns Quotient
 def div(x,y):
     return x / y
-
-def main():
+#Input
+def put():
+    global operation
     operation= input("What do you want to do? (+,-,*,/)")
 
     if(operation != "+" and operation != "-" and operation != "*" and operation != "/"):
         print ("You must enter a valid operation.")
+        put()
 
     else:
-        num1 = int(input("Enter First Number:"))
-        num2= int(input("Enter Second Number:"))
+        calc()
+#Number input
+def calc():
 
-        if(operation == "+"):
-            print (add(num1,num2))
+    num1 = int(input("Enter First Number:"))
+    num2= int(input("Enter Second Number:"))
 
-        elif(operaion == "-"):
-            print (sub(num1,num2))
+    if(operation == "+"):
+        print (add(num1,num2))
 
-        elif(operation == "*"):
-            print (mul(num1,num2))
+    elif(operaion == "-"):
+        print (sub(num1,num2))
 
-        else:
-            print (Dic(num1,num2))
+    elif(operation == "*"):
+        print (mul(num1,num2))
 
-main()
+    else:
+        print (div(num1,num2))
+
+put()
