1e0f8b79aa5cde323de37921cd1f2a661b612c69
Max Strehle <max-aero120@users.noreply.github.com>
2015-10-14 23:33:01 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+#Hello World!
+
+def helloWorld(myString):
+    print (myString)
+    myName= input("What is your name?")
+
+    if(myName =="Spencer"):
+        print ("Man you're ugly Spencer!")
+
+    else:
+        print ("Nice to meet you " + myName +".")
+
+print ("Hello World!")
+
+helloWorld("Hello Person.")
+
+helloWorld("Hello Next Person.")
